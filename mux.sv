83507643d3f2f81d9ee3eaf1dcbde8e83e00ece3
lmps-dev <lucasdemourapassossouza.dev@outlook.com>
2026-03-05 14:53:24 -0300

    i've figured out that the inputs have 32 bits lol

--- mux.sv
+++ mux.sv
@@ -1,16 +1,16 @@
-`timescale 1ns/1ps
-
 module mux (
-    output logic f,
-    input logic i0, i1, i2, i3, sel1, sel2
+    output logic [31:0] f,
+    input logic [31:0] i0, i1, i2, i3,
+    input logic [1:0] sel,
 );
-    and g0(f0, i0, sel1, sel2),
-            g1(f1, i1, not_sel1, sel2),
-            g2(f2, i2, sel1, not_sel2),
-            g3(f3, i3, not_sel1, not_sel2);
 
-    or g4(f, f0, f1, f2, f3);
-    not g5(not_sel1, sel1),
-        g6(not_sel2, sel2);
+    and g0 [31:0] (f0, i0, sel[0], sel[1]),
+        g1 [31:0] (f1, i1, not_sel0, sel[1]),
+        g2 [31:0] (f2, i2, sel[0], not_sel1),
+        g3 [31:0] (f3, i3, not_sel0, not_sel1);
+
+    or g4 [31:0] (f, f0, f1, f2, f3);
+    not g5 [31:0] (not_sel0, sel[0]),
+        g6 [31:0] (not_sel1, sel[1]);
 
 endmodule
--- tb_mux.sv
+++ tb_mux.sv
@@ -1,16 +1,16 @@
 `timescale 1ns/1ps
 
 module tb_mux;
-    logic [5:0] count;
-    logic muxOut;
+    logic [1:0] selector;
+    logic [31:0] muxOut;
+    logic [3:0] [31:0] inputs;
 
-    mux dut(.f(muxOut), .i0(count[0]), .i1(count[1]), .i2(count[2]), .i3(count[3]), .sel1(count[4]), .sel2(count[5]));
+    mux dut(.f(muxOut), .i0(inputs[0]), .i1(inputs[1]), .i2(inputs[2]), .i3(inputs[3]), .sel1(selector[0]), .sel2(selector[1]));
 
     initial begin
-        $monitor($time, "i0 = %b | i1 = %b | i2 = %b | i3 = %b | sel1 = %b | sel2 = %b | muxOut = %b", count[0], count[1], count[2], count[3], count[4], count[5], muxOut);
-        for (count = 0; count != 6'b111111; count++) #10;
+        $monitor($time, "i0 = %d | i1 = %d | i2 = %d | i3 = %d | sel1 = %b | sel2 = %b | muxOut = %d", inputs[0], inputs[1], inputs[2], inputs[3], selector[0], selector[1], muxOut);
 
-        #10 $stop;
+        $stop;
     end
 
 
