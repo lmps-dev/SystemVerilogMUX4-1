327505764d750704e21955b7e56910e476db84c6
veras-project <gav2@cin.ufpe.br>
2026-03-07 15:49:39 -0300

    feature: add comentário parciais

--- mux.sv
+++ mux.sv
@@ -1,12 +1,12 @@
 module mux (
-    output logic [31:0] f,
-    input logic [31:0] i0, i1, i2, i3,
-    input logic [1:0] sel
+    output logic [31:0] f, //Saída 32 bits
+    input logic [31:0] i0, i1, i2, i3, // Aqui são 4 variáveis de 32 bits
+    input logic [1:0] sel //Seletor entre 4 saídas
 );
-    logic [3:0] [31:0] arrayF;
+    logic [3:0] [31:0] arrayF; //A lógica é criar 4 vetores e cada um consegue armazenar 32 bits
     logic [1:0] not_sel;
 
-    for (genvar i = 0; i < 32; i = i + 1) begin
+    for (genvar i = 0; i < 32; i = i + 1) begin //Iteração para preencher criar as (32*4+32) portas necessárias
         and g1(arrayF[0][i], i0[i], not_sel[0], not_sel[1]),
             g2(arrayF[1][i], i1[i], sel[0], not_sel[1]),
             g3(arrayF[2][i], i2[i], not_sel[0], sel[1]),
@@ -15,7 +15,7 @@ module mux (
         or  g5(f[i], arrayF[0][i], arrayF[1][i], arrayF[2][i], arrayF[3][i]);    
     end
 
-    not g6(not_sel[0], sel[0]),
+    not g6(not_sel[0], sel[0]), // As negações declaradas
         g7(not_sel[1], sel[1]);
 
 endmodule
--- tb_mux.sv
+++ tb_mux.sv
@@ -1,24 +1,27 @@
 `timescale 1ns/1ps
 
 module tb_mux;
-    logic [1:0] selector;
-    logic [31:0] muxOut;
-    logic [3:0] [31:0] inputs;
+    // Repete a lógica do setup anterior.
+    logic [1:0] selector;  
+    logic [31:0] muxOut; 
+    logic [3:0] [31:0] inputs; 
 
-    mux dut(.f(muxOut), .i0(inputs[0]), .i1(inputs[1]), .i2(inputs[2]), .i3(inputs[3]), .sel(selector));
+    mux dut(.f(muxOut), .i0(inputs[0]), .i1(inputs[1]), .i2(inputs[2]), .i3(inputs[3]), .sel(selector)); //Responsável pela seleção das saídas em teste.
 
     initial begin
+        // Monitorar quando houver variação em algum desses 7 valores. As vezes binário as vezes decimal.
         $monitor($time, "i0 = %d | i1 = %d | i2 = %d | i3 = %d | sel[1] = %b | sel[0] = %b | muxOut = %d", inputs[0], inputs[1], inputs[2], inputs[3], selector[1], selector[0], muxOut);
 
-        inputs = '0; #10;
+        inputs = '0; #10; //inicializando 
         muxOut = '0; #10;
 
+        // Iteração para selecionar as saídas 
         for (int count = 0; count <= 1'b1; count++) begin
             for (int i = 0; i <= 2'b11; i++) begin
                 selector = i;
                 
                 for (int j = 0; j <= 2'b11; j++) begin
-                    inputs[j] = $urandom_range(0, 2**6) * 10; #10;
+                    inputs[j] = $urandom_range(0, 2**6) * 10; #10; // Função nativa de geração aleatória de valores de entrada.
                 
                 end
             end
